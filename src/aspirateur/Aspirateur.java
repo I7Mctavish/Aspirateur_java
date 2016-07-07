5985039a7b88b644d84bba694c86baa2f784e503
SylvainBallerini <ballerini.sylvain@gmail.com>
2016-07-07 15:37:20 +0200

    Projet Aspirateur OK

--- src/aspirateur/Aspirateur.java
+++ src/aspirateur/Aspirateur.java
@@ -2,50 +2,107 @@ package aspirateur;
 
 public class Aspirateur {
 	
-	//private points_cardinaux direction;
+	private String name;
 	private int position_X;
 	private int position_Y;
-	private String direction;
+	private points_cardinaux direction_enum = null;
+	private String directive;
 	
-	public Aspirateur(String position_debut,String directive){
-		
+	
+	public Aspirateur(String name, String position_debut,String directive){
+
 		String position_debut_tab[] = position_debut.split(" ");
-		for(String current : position_debut_tab)
-			System.out.println(current);
 		
-		for(int i =0;i<position_debut_tab.length;i++)
-			System.out.println("position_debut tab " + i + " = "+position_debut_tab[i]);
-		
-		
-		this.direction = position_debut_tab[2];
+		this.name = name;
 		this.position_X = Integer.parseInt(position_debut_tab[0]);
 		this.position_Y = Integer.parseInt(position_debut_tab[1]);
-		
-		System.out.println(this.getClass()+ " se trouve à la position X = "+this.position_X+" Y = "+this.position_Y
-				+" et dans la direction "+this.direction);
-		
-		
-	}
-		
-	public String getDirection() {
-		return direction;
-	}
-	public int getPosition_X() {
-		return position_X;
-	}
-	public int getPosition_Y() {
-		return position_Y;
+		this.directive = directive;
+		directionInitiale(position_debut_tab[2]);
+	}	
+	
+	private void directionInitiale(String direction_init){
+		if(direction_init.equals("N") )
+			this.direction_enum = points_cardinaux.N;
+		else if(direction_init.equals("E"))
+			this.direction_enum = points_cardinaux.E;
+		else if(direction_init.equals("S"))
+			this.direction_enum = points_cardinaux.S;
+		else if(direction_init.equals("W"))
+			this.direction_enum = points_cardinaux.W;
 	}
 	
-	public void setDirection(String direction) {
-		this.direction = direction;
+	// Méthode analyse qui va prendre chaque caractère de la variable String "directive" et qui fait un SWITCH en fonction de la lettre
+	
+	public void analyse_directive(){
+
+		for(int i = 0;i<this.directive.length();i++){
+			
+			switch(directive.charAt(i)){
+			case 'A':
+				move();
+				break;
+				
+			case 'D':
+				rotationDroite_enum();
+				break;
+
+			case 'G':
+				rotationGauche_enum();
+				break;
+			}
+		}
+
 	}
 	
-	public void setPosition_X(int position_X) {
-		this.position_X = position_X;
+	// En fonction de la direction la méthode move change l'attribut correspondant 
+	
+	private void move(){		
+	
+		if(this.direction_enum == points_cardinaux.N)
+			this.position_Y++;
+		
+		if(this.direction_enum == points_cardinaux.S)
+			this.position_Y--;
+		
+		if(this.direction_enum == points_cardinaux.E)
+			this.position_X++;
+		
+		if(this.direction_enum == points_cardinaux.W)
+			this.position_X--;
+		
+			
 	}
-	public void setPosition_Y(int position_Y) {
-		this.position_Y = position_Y;
+	
+	// Change la direction en fonction de la direction
+	private void rotationDroite_enum(){
+		
+		if(this.direction_enum == points_cardinaux.N)
+			this.direction_enum = points_cardinaux.E;
+		else if(this.direction_enum == points_cardinaux.E)
+			this.direction_enum = points_cardinaux.S;
+		else if(this.direction_enum == points_cardinaux.S)
+			this.direction_enum = points_cardinaux.W;
+		else if(this.direction_enum == points_cardinaux.W)
+			this.direction_enum = points_cardinaux.N;
+		
+	}
+	
+	// Change la direction en fonction de la direction
+	private void rotationGauche_enum(){
+		
+		if(this.direction_enum == points_cardinaux.N)
+			this.direction_enum = points_cardinaux.W;
+		else if(this.direction_enum == points_cardinaux.W)
+			this.direction_enum = points_cardinaux.S;
+		else if(this.direction_enum == points_cardinaux.S)
+			this.direction_enum = points_cardinaux.E;
+		else if(this.direction_enum == points_cardinaux.E)
+			this.direction_enum = points_cardinaux.N;
+		
+	}
+	
+	public void positionFinal(){
+		System.out.println(this.name + " " + this.position_X + " " + this.position_Y + " " + this.direction_enum);
 	}
 
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -10,41 +10,37 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		
+		// création de l'entré pour l'aplication
 		Collection <String> collection = new ArrayList<String>();
 		collection.add("10 10");
 		collection.add("5 5 N");
 		collection.add("DADADADAA");
 		
+		// initialisation des variables positions et directive
 		String position_debut=null;
 		String directive=null;
 		
-		
+		// séparation de l'entré pour différencier la taille de la grille, la position de départ, et les directives
 		for(String current : collection){
 			
 			String tab_temp_info[] = current.split(" ");
 						
-			if(tab_temp_info.length == 1){
-				System.out.println(" 1 => "+current);
-				directive = current;
-			}
-				
-			
-			if(tab_temp_info.length == 2)
-				System.out.println(" 2 => "+current);
-			
-			if(tab_temp_info.length == 3){
-				System.out.println(" 3 => "+current);
+			if(tab_temp_info.length == 1)
+				directive = current;							
+										
+			if(tab_temp_info.length == 3)
 				position_debut = current;
-				//System.out.println(position_debut);
-			}
-				
-			
-				
 		}
 		
-		Aspirateur Bobi = new Aspirateur(position_debut,directive);
-			
+		// instanciation de l'aspirateur Bobi avec son nom ça position et ses directives
+		Aspirateur Bobi = new Aspirateur("Bobi",position_debut,directive);
 		
+		// utilisation de la méthode analyse directive qui va lire .. les directives ( DADADADAA )
+		Bobi.analyse_directive();
+		
+		// position final
+		Bobi.positionFinal();
 
 	}
 
